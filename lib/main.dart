dc7c01a01110738fbb7ec7087eefe49b29113f80
AyushChothe <40957514+AyushChothe@users.noreply.github.com>
2023-01-21 23:56:55 +0530

    Device Preview Added

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:docker_remote/isar/server.dart';
 import 'package:docker_remote/pages/servers.dart';
 import 'package:docker_remote/providers/db.dart';
 import 'package:flex_color_scheme/flex_color_scheme.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -14,6 +15,7 @@ void main() async {
   final isar = await Isar.open([ServerSchema]);
 
   runApp(DevicePreview(
+    enabled: !kReleaseMode,
     builder: (_) => ProviderScope(
         overrides: [isarProvider.overrideWith((ref) => isar)],
         child: const MyApp()),
@@ -42,6 +44,7 @@ class MyApp extends StatelessWidget {
         fontFamily: GoogleFonts.notoSans().fontFamily,
       ),
       useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
       builder: DevicePreview.appBuilder,
       home: const ServersPage(),
       debugShowCheckedModeBanner: false,
